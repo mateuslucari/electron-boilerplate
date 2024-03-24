be1dab09c1a12e45f76d480f673f69dd6f4e77cd
Mateus Lucari <matheusfernandolisboa@gmail.com>
2024-03-24 18:25:01 -0300

    NodeIntegration desativado

--- core/app/init.ts
+++ core/app/init.ts
@@ -5,10 +5,10 @@ const isDev = !app.isPackaged;
 function createWindow() {
     // Create the browser window.
     let win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1000,
+        height: 800,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: false
         }
     });
 
